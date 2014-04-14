1d8806b2d710bc4ea7138ef0782608a980a9fdc5
@rafszul <rafszul@wearetheplaymakers.com>
2014-04-14 09:57:33 +0100

    bleach_v16_62
    1st release

---
+++ index_edgeActions.js
@@ -0,0 +1,314 @@
+
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${_cyberpunkNowLogoFist002-02}","click",function(sym,e){sym.play();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_cyberpunkNowLogoFist002-02}","touchend",function(sym,e){sym.play();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_cyberpunkNowLogoFist002-02}","swipeleft",function(sym,e){sym.play();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Text2Copy13}","click",function(sym,e){window.open("http://bleachv1662.tumblr.com/","_null");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Text2Copy13}","dblclick",function(sym,e){window.open("http://bleachv1662.tumblr.com/","_null");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Text2Copy13}","touchend",function(sym,e){window.open("http://bleachv1662.tumblr.com/","_null");});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",44999,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",49000,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",54250,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",58244,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",60000,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",63003,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",66750,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",72497,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",75753,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",80004,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",81996,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",84007,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",85999,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",88512,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",93000,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",97001,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",69002,function(sym,e){sym.stop();});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+
+//=========================================================
+
+//Edge symbol: 'p001'
+(function(symbolName){})("p001");
+//Edge symbol end:'p001'
+
+//=========================================================
+
+//Edge symbol: 'titleCredits001'
+(function(symbolName){})("titleCredits001");
+//Edge symbol end:'titleCredits001'
+
+//=========================================================
+
+//Edge symbol: 'euAltLogo'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",15000,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Text5}","click",function(sym,e){window.open("https://twitter.com/search?src=typd&q=%23transeuropacaravans","_null");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Text5}","dblclick",function(sym,e){window.open("https://twitter.com/search?src=typd&q=%23transeuropacaravans","_null");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Text5}","touchend",function(sym,e){window.open("https://twitter.com/search?src=typd&q=%23transeuropacaravans","_null");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_euAltLogo-01}","click",function(sym,e){window.open("https://twitter.com/EuroAlter","_null");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_euAltLogo-01}","dblclick",function(sym,e){window.open("https://twitter.com/EuroAlter","_null");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_euAltLogo-01}","touchend",function(sym,e){window.open("https://twitter.com/EuroAlter","_null");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_transEuroCarLogo}","click",function(sym,e){window.open("http://citizenspact.eu/","_null");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_transEuroCarLogo}","dblclick",function(sym,e){window.open("http://citizenspact.eu/","_null");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_transEuroCarLogo}","touchend",function(sym,e){window.open("http://citizenspact.eu/","_null");});
+//Edge binding end
+})("euAltLogo");
+//Edge symbol end:'euAltLogo'
+
+//=========================================================
+
+//Edge symbol: 'euBurningTitle'
+(function(symbolName){})("euBurningTitle");
+//Edge symbol end:'euBurningTitle'
+
+//=========================================================
+
+//Edge symbol: 'roadSVG'
+(function(symbolName){})("roadSVG");
+//Edge symbol end:'roadSVG'
+
+//=========================================================
+
+//Edge symbol: 'quoteTJ'
+(function(symbolName){})("quoteTJ");
+//Edge symbol end:'quoteTJ'
+
+//=========================================================
+
+//Edge symbol: 'wATPMImpCreaIntro'
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${_wATPMlogoTrans119x1233Col-03}","click",function(sym,e){window.open("https://www.facebook.com/weAreThePlaymakers/","_null");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_wATPMlogoTrans119x1233Col-03}","dblclick",function(sym,e){window.open("https://www.facebook.com/weAreThePlaymakers/","_null");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_wATPMlogoTrans119x1233Col-03}","touchend",function(sym,e){window.open("https://www.facebook.com/weAreThePlaymakers/","_null");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_impertinentCreation}","click",function(sym,e){window.open("http://impertinentcreations.com/","_null");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_impertinentCreation}","dblclick",function(sym,e){window.open("http://impertinentcreations.com/","_null");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_impertinentCreation}","touchend",function(sym,e){window.open("http://impertinentcreations.com/","_null");});
+//Edge binding end
+})("wATPMImpCreaIntro");
+//Edge symbol end:'wATPMImpCreaIntro'
+
+//=========================================================
+
+//Edge symbol: 'textSymbol'
+(function(symbolName){})("textSymbol");
+//Edge symbol end:'textSymbol'
+
+//=========================================================
+
+//Edge symbol: 'p002'
+(function(symbolName){})("p002");
+//Edge symbol end:'p002'
+
+//=========================================================
+
+//Edge symbol: 'p003'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",52500,function(sym,e){});
+//Edge binding end
+})("p003");
+//Edge symbol end:'p003'
+
+//=========================================================
+
+//Edge symbol: 'p004'
+(function(symbolName){})("p004");
+//Edge symbol end:'p004'
+
+//=========================================================
+
+//Edge symbol: 'p005'
+(function(symbolName){})("p005");
+//Edge symbol end:'p005'
+
+//=========================================================
+
+//Edge symbol: 'p006'
+(function(symbolName){})("p006");
+//Edge symbol end:'p006'
+
+//=========================================================
+
+//Edge symbol: 'p007'
+(function(symbolName){})("p007");
+//Edge symbol end:'p007'
+
+//=========================================================
+
+//Edge symbol: 'p008'
+(function(symbolName){})("p008");
+//Edge symbol end:'p008'
+
+//=========================================================
+
+//Edge symbol: 'p008_1'
+(function(symbolName){})("p008_1");
+//Edge symbol end:'p008_1'
+
+//=========================================================
+
+//=========================================================
+
+//Edge symbol: 'p010'
+(function(symbolName){})("p010");
+//Edge symbol end:'p010'
+
+//=========================================================
+
+//Edge symbol: 'p011'
+(function(symbolName){})("p011");
+//Edge symbol end:'p011'
+
+//=========================================================
+
+//Edge symbol: 'cardHolder'
+(function(symbolName){})("cardHolder");
+//Edge symbol end:'cardHolder'
+
+//=========================================================
+
+//Edge symbol: 'p012'
+(function(symbolName){})("p012");
+//Edge symbol end:'p012'
+
+//=========================================================
+
+//Edge symbol: 'p013'
+(function(symbolName){})("p013");
+//Edge symbol end:'p013'
+
+//=========================================================
+
+//Edge symbol: 'p014'
+(function(symbolName){})("p014");
+//Edge symbol end:'p014'
+
+//=========================================================
+
+//Edge symbol: 'p015'
+(function(symbolName){})("p015");
+//Edge symbol end:'p015'
+
+//=========================================================
+
+//Edge symbol: 'p016'
+(function(symbolName){})("p016");
+//Edge symbol end:'p016'
+
+//=========================================================
+
+//=========================================================
+
+//Edge symbol: 'Symbol_1'
+(function(symbolName){})("logo550x784");
+//Edge symbol end:'logo550x784'
+
+//=========================================================
+
+//Edge symbol: 'cardQR'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",500,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Group3}","mouseover",function(sym,e){sym.play("clip");});
+//Edge binding end
+})("cardQRBleachTumblr");
+//Edge symbol end:'cardQRBleachTumblr'
+
+//=========================================================
+
+//Edge symbol: 'cardQR_1'
+(function(symbolName){})("cardQR_1EuAlt");
+//Edge symbol end:'cardQR_1EuAlt'
+
+//=========================================================
+
+//Edge symbol: 'cardQR_1EuAlt_1'
+(function(symbolName){})("cardQR_1wATPM");
+//Edge symbol end:'cardQR_1wATPM'
+
+//=========================================================
+
+//Edge symbol: 'cardQR_1wATPM_1'
+(function(symbolName){})("cardQR_1WildRumpus");
+//Edge symbol end:'cardQR_1WildRumpus'
+
+//=========================================================
+
+//Edge symbol: 'cardQR_1WildRumpus_1'
+(function(symbolName){})("cardQR_1Kozilek");
+//Edge symbol end:'cardQR_1Kozilek'
+
+//=========================================================
+
+//Edge symbol: 'qrCodes'
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${_cardQR_1Kozilek}","dblclick",function(sym,e){window.open("http://havingfunwithrecords.com/kozilek/","_null");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_cardQR_1wATPM}","dblclick",function(sym,e){window.open("http://wearetheplaymakers.com/","_null");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_cardQR_1EuAlt}","dblclick",function(sym,e){window.open("http://www.euroalter.com/","_null");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_cardQR_1WildRumpus}","click",function(sym,e){window.open("http://thewildrumpus.co.uk/","_null");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_cardQRBleachTumblr}","dblclick",function(sym,e){window.open("http://bleachv1662.tumblr.com/","_null");});
+//Edge binding end
+})("qrCodes");
+//Edge symbol end:'qrCodes'
+
+//=========================================================
+
+//Edge symbol: 'text'
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${_Text2}","dblclick",function(sym,e){window.open("https://twitter.com/YochaiBenkler","_null");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Text2}","click",function(sym,e){window.open("https://twitter.com/YochaiBenkler","_null");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Text2}","touchend",function(sym,e){window.open("https://twitter.com/YochaiBenkler","_null");});
+//Edge binding end
+})("text");
+//Edge symbol end:'text'
+
+//=========================================================
+
+//Edge symbol: 'roadSVGBack'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",933,function(sym,e){sym.play("road");});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){sym.play();});
+//Edge binding end
+})("roadSVGBack");
+//Edge symbol end:'roadSVGBack'
+})(jQuery,AdobeEdge,"EDGE-27978891");
